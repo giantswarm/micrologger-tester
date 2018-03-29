19ac43b5e98c297fb14d20d286298301ce803edc
Julien Garcia Gonzalez <julien@giantswarm.io>
2018-03-29 08:56:54 +0200

    fix build
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- main.go
+++ main.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	logger, err := micrologger.New(micrologger.Config{})
-	if err != nil {
-		panic(microerror.Mask(err))
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		{
+			Name: "version",
+			Action: func(c *cli.Context) error {
+				fmt.Println("No compiled version")
+				return nil
+			},
+		},
 	}
 
-	i := 0
-	for range time.Tick(1 * time.Second) {
-		logger.Log("level", "info", "message", fmt.Sprintf("Generating entry %v", i))
-		i++
+	app.Action = func(c *cli.Context) error {
+
+		logger, err := micrologger.New(micrologger.Config{})
+		if err != nil {
+			panic(microerror.Mask(err))
+		}
+
+		i := 0
+		for range time.Tick(1 * time.Second) {
+			logger.Log("level", "info", "message", fmt.Sprintf("Generating entry %v", i))
+			i++
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
